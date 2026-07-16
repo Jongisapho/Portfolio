76e9a825becadd286034fec7f5d137b79656361a
Jongisapho Ndeya <u22625926@tuks.co.za>
2026-07-16 20:54:04 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="min-h-screen bg-linear-to-br from-cyan-700 via-gray-200 to-cyan-300">
+    <Navbar />
+  </div>
+</template>
+<script>
+
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components : {
+    Navbar,
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="flex justify-between p-6 bg-opacity-50 relative z-20">
+        <div class="text-3xl font-bold bg-linear-to-r from-gray-700 to-gray-600 bg-clip-text text-transparent">
+            Dev <span class="font-black"> Portfolio </span>
+        </div>
+        <div class="md:hidden z-30">
+            <button type="button"
+                class="block focus:outline-none p-2 rounded-lg bg">
+            </button>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+
+} 
+</script>
+<style>
+
+</style>
